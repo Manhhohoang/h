5e35c5ca86804e2965279dff536430d77e68d860
ProCoderMew <mauminh.nguyen@gmail.com>
2021-07-23 20:12:49 +0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # storage-data
 Script muon tu Huybopbi
+
+Nhap vao termux dong sau de bat dau cai bot
+
+```sh
+    termux-setup-storage && apt update && apt upgrade && pkg install curl -y && bash <(curl -s https://raw.githubusercontent.com/ProCoderMew/storage-data/ProCoderMew/install.sh)
+```
+
+Chinh sua data tai package.json
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+cat << "EOF"
+
+Special thanks to:
+  Phạm Quang Huy(huybopbi) for this script.
+
+EOF
+read
+
+echo "==============================================="
+echo "[!] Tiến hành cài đặt các gói library còn thiếu [!]"
+pkg install -y libpixman libcairo pango xorgproto php libjpeg-turbo-progs libjpeg-turbo-static libjpeg-turbo librsvg librsvg-static nodejs git python wget
+
+git clone https://github.com/ProCoderMew/OneFile
+cd OneFile
+echo "[!] Đã tải source code thành công, tiến hành cài các gói cần thiết [!]"
+npm install
+echo "[!] Đã cài các gói cần thiết thành công, tiến hành cài đặt file manager [!]"
+bash <(wget -qO- https://raw.githubusercontent.com/ProCoderMew/storage-data/ProCoderMew/install-manager.sh)
+echo "[!] Đã cài đặt file manager thành công [!]"
+
+echo "==============================================="
+echo "Cài đặt hoàn tất! Hãy sửa file package.json, thêm cookies ở cửa sổ sau."
+echo "Toàn bộ thư mục của bot được lưu tại '/home/OneFile'"
+echo "Nhấn enter để mở file manager."
+read
+
+manager
