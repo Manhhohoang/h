22ffe5c318a0c25731c61872d111da20a85033c3
Manhhohoang <86575029+Manhhohoang@users.noreply.github.com>
2021-08-02 14:23:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,14 +16,12 @@ pkg install -y php nodejs git wget
 git clone https://github.com/Manhhohoang/a.git
 cd a
 echo "[!] Đã tải source code thành công, tiến hành cài các gói cần thiết [!]"
-npm install
-npm i sqlite3
 echo "[!] Đã cài các gói cần thiết thành công, tiến hành cài đặt file manager [!]"
 bash <(wget -qO- https://raw.githubusercontent.com/Manhhohoang/h/ProCoderMew/install-manager.sh)
 echo "[!] Đã cài đặt file manager thành công [!]"
 
 echo "==============================================="
-echo "Cài đặt hoàn tất! Hãy sửa file package.json, thêm cookies ở cửa sổ sau."
+echo "Cài đặt hoàn tất! Hãy sửa ở cửa sổ sau."
 echo "Toàn bộ thư mục của bot được lưu tại '/home/a'"
 echo "Nhấn enter để mở file manager."
 read
