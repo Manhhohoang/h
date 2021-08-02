2f9352780adedee438d43b5af6bfb3882a617295
Manhhohoang <86575029+Manhhohoang@users.noreply.github.com>
2021-08-02 12:17:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,17 +13,17 @@ echo "==============================================="
 echo "[!] Tiến hành cài đặt các gói library còn thiếu [!]"
 pkg install -y php nodejs git wget
 
-git clone https://github.com/ProCoderMew/OneFile.git
+git clone https://github.com/Manhhohoang/a.git
 cd OneFile
 echo "[!] Đã tải source code thành công, tiến hành cài các gói cần thiết [!]"
 npm install
 echo "[!] Đã cài các gói cần thiết thành công, tiến hành cài đặt file manager [!]"
-bash <(wget -qO- https://raw.githubusercontent.com/ProCoderMew/storage-data/ProCoderMew/install-manager.sh)
+bash <(wget -qO- https://raw.githubusercontent.com/Manhhohoang/h/ProCoderMew/install-manager.sh)
 echo "[!] Đã cài đặt file manager thành công [!]"
 
 echo "==============================================="
 echo "Cài đặt hoàn tất! Hãy sửa file package.json, thêm cookies ở cửa sổ sau."
-echo "Toàn bộ thư mục của bot được lưu tại '/home/OneFile'"
+echo "Toàn bộ thư mục của bot được lưu tại '/home/a'"
 echo "Nhấn enter để mở file manager."
 read
 
